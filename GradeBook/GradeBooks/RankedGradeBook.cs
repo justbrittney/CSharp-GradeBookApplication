4399b4c289c5d82a8fcd184bcc36464cc41400fa
justbrittney <justbrittney486@gmail.com>
2022-09-05 14:24:22 -0400

    Creating new gradebook types

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook (string name): base(name)
+        {
+            base.Type = Enums.GradebookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+       public StandardGradeBook(string name): base(name)
+        {
+            base.Type = GradebookType.Standard;
+        }
+
+    }
+}
+ 
